9a0c7853988558c4813e8aa503f9d621ed1bfef4
Ana Luisa Silva do Amaral <contato.analusilvamarau@gmail.com>
2024-06-26 21:41:30 -0300

    bola

--- bruxa.py
+++ bruxa.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import os
 from tkinter import simpledialog
+from extras.funcoes import LimparTela
 
 pygame.init()
 
---
+++ extras/funcoes.py
@@ -0,0 +1,3 @@
+import os
+def LimparTela():
+    os.system("cls")
